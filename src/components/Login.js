50802f89ca0923d10da54c4be75a2cb917eb1b93
aashish-cd <076bct002@ioepc.edu.np>
2022-06-09 19:02:15 +0545

    image added

--- src/components/Login.js
+++ src/components/Login.js
@@ -87,7 +87,7 @@ const Login = () => {
 
       <img
         className='image'
-        src='image.jpg'
+        src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
         width={'70%'}
         alt='night view of dharan nepal'
       />
